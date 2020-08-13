0d98de8378a9c4475e3f37658e92ad93f0942514
mbeverly18 <64284566+mbeverly18@users.noreply.github.com>
2020-08-13 18:04:22 -0500

    corrected errors
    working now

--- main.js
+++ main.js
@@ -67,7 +67,7 @@ while (balls.length < 25) {
     random(0 + size, height - size),
     random(-7,7),
     random(-7,7),
-    'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) + ',', 
+    'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) + ')', 
     size
   );
 
